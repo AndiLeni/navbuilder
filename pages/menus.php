2bd6bb2494fa85a30232cc90a51052fe6da49dae
Andreas Lenhardt <andi.lenhardt@googlemail.com>
2023-10-28 19:39:49 +0200

    Apply patch from https://github.com/FriendsOfREDAXO/navbuilder/pull/17

--- pages/menus.php
+++ pages/menus.php
@@ -4,79 +4,79 @@ $id = rex_request('id', 'int');
 $func = rex_request('func', 'string');
 
 $nav = rex_post('config', [
-    ['name', 'string'],
-    ['structure', 'string'],
+	['name', 'string'],
+	['structure', 'string'],
 ]);
 
 if ($func === "save") {
 
-    $sql = rex_sql::factory();
-    $sql->setTable(rex::getTable("navbuilder_navigation"));
+	$sql = rex_sql::factory();
+	$sql->setTable(rex::getTable("navbuilder_navigation"));
 
-    if ($id > 0) {
-        $sql->setWhere('id="' . $id . '"');
-        $sql->setValue('name', $nav['name']);
-        $sql->setValue('structure', $nav['structure']);
-        $sql->update();
-        echo rex_view::success('Navigation "' . $nav['name'] . '" wurde aktualisiert');
-    } else {
-        $sql->setValue('name', $nav['name']);
-        $sql->setValue('structure', $nav['structure']);
-        $sql->insert();
-        $id = (int)$sql->getLastId();
-        echo rex_view::success('Navigation "' . $nav['name'] . '" wurde angelegt');
-    }
+	if ($id > 0) {
+		$sql->setWhere('id="' . $id . '"');
+		$sql->setValue('name', $nav['name']);
+		$sql->setValue('structure', $nav['structure']);
+		$sql->update();
+		echo rex_view::success('Navigation "' . $nav['name'] . '" wurde aktualisiert');
+	} else {
+		$sql->setValue('name', $nav['name']);
+		$sql->setValue('structure', $nav['structure']);
+		$sql->insert();
+		$id = (int)$sql->getLastId();
+		echo rex_view::success('Navigation "' . $nav['name'] . '" wurde angelegt');
+	}
 }
 
 if ($func === "delete") {
 
-    $sql = rex_sql::factory();
-    $sql->setTable(rex::getTable("navbuilder_navigation"));
-    $sql->setWhere('id="' . $id . '"');
-    $sql->delete();
+	$sql = rex_sql::factory();
+	$sql->setTable(rex::getTable("navbuilder_navigation"));
+	$sql->setWhere('id="' . $id . '"');
+	$sql->delete();
 
-    echo rex_view::success('Navigation "' . $nav['name'] . '" wurde gelöscht');
+	echo rex_view::success('Navigation "' . $nav['name'] . '" wurde gelöscht');
 }
 
 if ($func == '' || $func == 'delete') {
-    $list = rex_list::factory("SELECT `id`, `name`, CONCAT('REX_NAVBUILDER[name=',`name`,']') as `snippet` FROM `" . rex::getTablePrefix() . "navbuilder_navigation` ORDER BY `name` ASC");
-    $list->addTableAttribute('class', 'table-striped');
+	$list = rex_list::factory("SELECT `id`, `name`, CONCAT('REX_NAVBUILDER[name=',`name`,']') as `snippet` FROM `" . rex::getTablePrefix() . "navbuilder_navigation` ORDER BY `name` ASC");
+	$list->addTableAttribute('class', 'table-striped');
 
-    // icon column
-    $thIcon = '<a href="' . $list->getUrl(['func' => 'add']) . '"><i class="rex-icon rex-icon-add-action"></i></a>';
-    $tdIcon = '<i class="rex-icon fa-file-text-o"></i>';
-    $list->addColumn($thIcon, $tdIcon, 0, ['<th class="rex-table-icon">###VALUE###</th>', '<td class="rex-table-icon">###VALUE###</td>']);
-    $list->setColumnParams($thIcon, ['func' => 'edit', 'id' => '###id###']);
+	// icon column
+	$thIcon = '<a href="' . $list->getUrl(['func' => 'add']) . '"><i class="rex-icon rex-icon-add-action"></i></a>';
+	$tdIcon = '<i class="rex-icon fa-file-text-o"></i>';
+	$list->addColumn($thIcon, $tdIcon, 0, ['<th class="rex-table-icon">###VALUE###</th>', '<td class="rex-table-icon">###VALUE###</td>']);
+	$list->setColumnParams($thIcon, ['func' => 'edit', 'id' => '###id###']);
 
-    $list->setColumnLabel('name', 'Name');
-    $list->setColumnLabel('snippet', 'Snippet');
+	$list->setColumnLabel('name', 'Name');
+	$list->setColumnLabel('snippet', 'Snippet');
 
-    $list->setColumnParams('name', ['id' => '###id###', 'func' => 'edit']);
+	$list->setColumnParams('name', ['id' => '###id###', 'func' => 'edit']);
 
-    $list->removeColumn('id');
+	$list->removeColumn('id');
 
-    $content = $list->get();
+	$content = $list->get();
 
-    $fragment = new rex_fragment();
-    $fragment->setVar('content', $content, false);
-    $content = $fragment->parse('core/page/section.php');
+	$fragment = new rex_fragment();
+	$fragment->setVar('content', $content, false);
+	$content = $fragment->parse('core/page/section.php');
 
-    echo $content;
+	echo $content;
 } else if ($func == 'add' || $func == 'edit' || $func == 'save') {
 
-    $widget = rex_var_link::getWidget('href', 'href', 1);
+	$widget = rex_var_link::getWidget('href', 'href', 1);
 
-    $content = '';
+	$content = '';
 
-    $nav = rex_navbuilder_navigation::create();
+	$nav = rex_navbuilder_navigation::create();
 
-    if ($id > 0) {
-        $nav = rex_navbuilder_navigation::get($id);
-    }
+	if ($id > 0) {
+		$nav = rex_navbuilder_navigation::get($id);
+	}
 
-    $content .= '
+	$content .= '
         <script>
-			var navbuilderJson = ' . ($nav->structure != '' ? $nav->structure : '{}') . ';
+			var navbuilderJson = ' . ($nav->hasValue('structure') ? $nav->structure : '{}') . ';
         </script>
 		<div class="row">
 			<form id="frmOut" action="' . rex_url::currentBackendPage() . '" method="post">
@@ -89,7 +89,7 @@ if ($func == '' || $func == 'delete') {
 							<div class="form-group">
 								<label for="name" class="col-sm-2 control-label">Name</label>
 								<div class="col-sm-10">
-									<input id="name" type="text" name="config[name]" value="' . $nav->name . '"/>
+									<input id="name" type="text" name="config[name]" value="' . ($nav->hasValue('name') ? $nav->name : '') . '"/>
 								</div>
 							</div>
 						</div>
@@ -187,10 +187,9 @@ if ($func == '' || $func == 'delete') {
 		</div>
         ';
 
-    $fragment = new rex_fragment();
-    $fragment->setVar('class', 'edit');
-    $fragment->setVar('title', 'Einstellungen');
-    $fragment->setVar('body', $content, false);
-    echo $fragment->parse('core/page/section.php');
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'edit');
+	$fragment->setVar('title', 'Einstellungen');
+	$fragment->setVar('body', $content, false);
+	echo $fragment->parse('core/page/section.php');
 }
-?>
